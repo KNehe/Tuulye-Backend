15f89babd52eaa656cd4e72b16269f98cbd2528b
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2020-02-20 18:59:19 +0300

    Eslint, prettier setup

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,66 +1,65 @@
-import AppError from "../Utils/appError";
+import AppError from '../Utils/appError';
 
-const handleValidationErrorDB = err =>{
-    const errors = Object.values(err.errors).map(el => el.message);
-    const message = `Invalid input data ${errors.join('. ')}`;
-    return new AppError(message,404);
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data ${errors.join('. ')}`;
+  return new AppError(message, 404);
 };
 
-const handleCastErrorDB = err =>{
-    const message = `Invalid ${err.path}: ${err.value}.`;
-    return new AppError(message,404);
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 404);
 };
 
-const handleDuplicateFieldsDB = err =>{
-    const values = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-    const message = `Duplicate field ${values} : Enter another value`;
-    return new AppError(message,404);
+const handleDuplicateFieldsDB = (err) => {
+  const values = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field ${values} : Enter another value`;
+  return new AppError(message, 404);
 };
 
-const sendDev = (err,res)=>{
+const sendDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
 
+const sendProd = (err, res) => {
+  // Known cause from code
+  if (err.isOperational) {
     res.status(err.statusCode).json({
-        status:err.status,
-        err:err,
-        message:err.message,
-        stack:err.stack
-    });  
+      status: err.status,
+      message: err.message,
+    });
+  } else { // unknown
+    console.log(err);
+    res.status(500).json({
+      status: 'error',
+      message: 'SOMETHING BAD HAPPENED',
+    });
+  }
 };
 
-const sendProd = (err,res)=>{
-     //Known cause from code
-    if(err.isOperational){
-        res.status(err.statusCode).json({
-            status:err.status,
-            message:err.message
-        });
 
-    }else{ // unknown
-        console.log(err);
-        res.status(500).json({
-            status:"error",
-            message:"SOMETHING BAD HAPPENED"
-        });
-    }
+const globalErrorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.code === 'ValidationError') error = handleValidationErrorDB(error);
+
+    sendProd(error, res);
+  }
 };
 
-const globalErrorHandler = (err,req,res,next)=>{
-   
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
 
-    if(process.env.NODE_ENV === "development")
-    {
-        sendDev(err,res);
-    }else if (process.env.NODE_ENV === "production"){
-        let error = {...err};
-
-        if(error.name === "CastError") error = handleCastErrorDB(error);
-        if(error.code === 11000) error = handleDuplicateFieldsDB(error);
-        if(error.code === "ValidationError") error = handleValidationErrorDB(error);
-
-        sendProd(error,res);
-    }
-};
 
 export default globalErrorHandler;
